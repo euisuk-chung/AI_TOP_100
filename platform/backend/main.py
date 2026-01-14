c0d9c9db50daec1c40c480ce3212fe5c507252ef
euisuk.chung <jasuchung@gmail.com>
2026-01-15 08:04:23 +0900

    chore: Update backend server and frontend proxy to use port 8001.

--- platform/backend/main.py
+++ platform/backend/main.py
@@ -329,4 +329,4 @@ def get_solution(filename: str):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8001)
